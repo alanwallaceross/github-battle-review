37e509e6d678c9eb0c873b396d885cf3591a7b2d
Alan Ross <alanross2006@gmail.com>
2020-03-10 21:55:12 +0000

    just finished player preview

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Popular from "./components/Popular";
+import Battle from "./components/Battle";
+
+function App() {
+  return (
+    <div>
+      <Battle />
+    </div>
+  );
+}
+
+export default App;
